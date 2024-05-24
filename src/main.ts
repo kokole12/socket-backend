9754844afd33faae67f6e9d680a2d97d12da5c16
kokole Ismail <kokoleismail886@gmail.com>
2024-05-24 13:41:24 +0300

    websockets in nest js

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+  await app.listen(3000);
+}
+bootstrap();
